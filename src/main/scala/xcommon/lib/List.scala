d3c8e3d7697dfb7fde26e03fa730e8b3dfe6bac6
sang dang <dnsang1010@gmail.com>
2020-03-02 10:41:34 +0700

    add list & idgenerator

---
+++ src/main/scala/xcommon/lib/List.scala
@@ -0,0 +1,24 @@
+package xcommon.lib
+
+import scala.concurrent.Future
+
+trait List[T] {
+
+  def pushFront(value: T): Future[Boolean]
+
+  def pushBack(value: T): Future[Boolean]
+
+  def size(): Future[Option[Int]]
+
+  def popFront(): Future[Option[T]]
+
+  def popBack(): Future[Option[T]]
+
+  def clear(): Future[Boolean]
+
+  def get(index: Int): Future[Option[T]]
+
+  def get(from: Int, num: Int): Future[Option[Array[T]]]
+
+  def set(index: Int, value: T): Future[Boolean]
+}
